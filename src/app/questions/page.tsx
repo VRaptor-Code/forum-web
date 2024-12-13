3d48a5bc378de221a17339b3a2af94186476db19
douglas <douglasmartinssantos7@gmail.com>
2024-12-13 17:49:52 -0300

    style: change single quotes to double quotes for consistency

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -5,63 +5,65 @@ import { useRouter } from 'next/navigation' // Importe o useRouter
 import { Container, Typography, TextField, Button } from '@mui/material'
 
 const QuestionsPage = () => {
-  const [newQuestion, setNewQuestion] = useState('')
-  const router = useRouter() // Inicialize o useRouter
+    const [newQuestion, setNewQuestion] = useState('')
+    const router = useRouter() // Inicialize o useRouter
 
-  const handleAddQuestion = () => {
-    if (newQuestion.trim()) {
-      // Redireciona para /topicoList com a pergunta como parâmetro de consulta
-      router.push(`/topicoList?pergunta=${encodeURIComponent(newQuestion)}`)
+    const handleAddQuestion = () => {
+        if (newQuestion.trim()) {
+            // Redireciona para /topicoList com a pergunta como parâmetro de consulta
+            router.push(
+                `/topicoList?pergunta=${encodeURIComponent(newQuestion)}`,
+            )
+        }
     }
-  }
 
-  return (
-    <Container
-      maxWidth="md"
-      sx={{
-        bgcolor: '#121212',
-        color: '#ffffff',
-        minHeight: '100vh',
-        padding: 4,
-      }}
-    >
-      <Typography variant="h4" align="center" sx={{ mb: 4 }}>
-        Faça uma Pergunta
-      </Typography>
+    return (
+        <Container
+            maxWidth="md"
+            sx={{
+                bgcolor: '#121212',
+                color: '#ffffff',
+                minHeight: '100vh',
+                padding: 4,
+            }}
+        >
+            <Typography variant="h4" align="center" sx={{ mb: 4 }}>
+                Faça uma Pergunta
+            </Typography>
 
-      <TextField
-        label="Digite sua pergunta"
-        variant="outlined"
-        fullWidth
-        value={newQuestion}
-        onChange={(e) => setNewQuestion(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            e.preventDefault()
-            handleAddQuestion()
-          }
-        }}
-        sx={{
-          input: { color: '#ffffff' },
-          '.MuiOutlinedInput-root': {
-            '& fieldset': { borderColor: '#ffffff' },
-            '&:hover fieldset': { borderColor: '#ff6f00' },
-            '&.Mui-focused fieldset': { borderColor: '#ff6f00' },
-          },
-          style: { color: '#ffffff' },
-          mb: 2,
-        }}
-      />
+            <TextField
+                label="Digite sua pergunta"
+                variant="outlined"
+                fullWidth
+                value={newQuestion}
+                onChange={(e) => setNewQuestion(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        e.preventDefault()
+                        handleAddQuestion()
+                    }
+                }}
+                sx={{
+                    input: { color: '#ffffff' },
+                    '.MuiOutlinedInput-root': {
+                        '& fieldset': { borderColor: '#ffffff' },
+                        '&:hover fieldset': { borderColor: '#ff6f00' },
+                        '&.Mui-focused fieldset': { borderColor: '#ff6f00' },
+                    },
+                    style: { color: '#ffffff' },
+                    mb: 2,
+                }}
+            />
 
-      <Button
-        variant="contained"
-        onClick={handleAddQuestion}
-        sx={{ backgroundColor: '#ff6f00' }}
-      >
-        Confirmar
-      </Button>
-    </Container>
-  )
+            <Button
+                variant="contained"
+                onClick={handleAddQuestion}
+                sx={{ backgroundColor: '#ff6f00' }}
+            >
+                Confirmar
+            </Button>
+        </Container>
+    )
 }
 
 export default QuestionsPage
--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,63 +1,65 @@
 'use client'
 
-import { useState } from 'react'
 import {
-  Container,
-  Typography,
-  List,
-  ListItem,
-  ListItemText,
+    Container,
+    Typography,
+    List,
+    ListItem,
+    ListItemText,
 } from '@mui/material'
 
 interface TopicoPorCategoriaDto {
-  categoria: string
-  quantidade: number
+    categoria: string
+    quantidade: number
 }
 
 function Report() {
-  const [topicosPorCategoria, setTopicosPorCategoria] = useState<
-    TopicoPorCategoriaDto[]
-  >([
-    { categoria: 'Kotlin', quantidade: 15 },
-    { categoria: 'React', quantidade: 22 },
-    { categoria: 'Angular', quantidade: 8 },
-    { categoria: 'Next.js', quantidade: 12 },
-    { categoria: 'JavaScript', quantidade: 30 },
-    { categoria: 'TypeScript', quantidade: 25 },
-    { categoria: 'Java', quantidade: 10 },
-    { categoria: 'Python', quantidade: 18 },
-  ])
+    const topicosPorCategoria: TopicoPorCategoriaDto[] = [
+        { categoria: 'Kotlin', quantidade: 15 },
+        { categoria: 'React', quantidade: 22 },
+        { categoria: 'Angular', quantidade: 8 },
+        { categoria: 'Next.js', quantidade: 12 },
+        { categoria: 'JavaScript', quantidade: 30 },
+        { categoria: 'TypeScript', quantidade: 25 },
+        { categoria: 'Java', quantidade: 10 },
+        { categoria: 'Python', quantidade: 18 },
+    ]
 
-  return (
-    <Container
-      maxWidth="sm"
-      sx={{
-        bgcolor: '#121212',
-        color: '#ffffff',
-        minHeight: '100vh',
-        padding: 4,
-      }}
-    >
-      <Typography variant="h4" align="center" sx={{ mb: 4 }}>
-        Relatório de Tópicos por Categoria
-      </Typography>
+    return (
+        <Container
+            maxWidth="sm"
+            sx={{
+                bgcolor: '#121212',
+                color: '#ffffff',
+                minHeight: '100vh',
+                padding: 4,
+            }}
+        >
+            <Typography variant="h4" align="center" sx={{ mb: 4 }}>
+                Relatório de Tópicos por Categoria
+            </Typography>
 
-      <List>
-        {topicosPorCategoria.map((item) => (
-          <ListItem
-            key={item.categoria}
-            sx={{ bgcolor: '#1e1e1e', borderRadius: 2, mb: 1, p: 2 }}
-          >
-            <ListItemText
-              primary={item.categoria}
-              secondary={`Quantidade: ${item.quantidade}`}
-              sx={{ color: 'inherit' }} // Define a cor do texto como padrão (branco neste caso)
-            />
-          </ListItem>
-        ))}
-      </List>
-    </Container>
-  )
+            <List>
+                {topicosPorCategoria.map((item) => (
+                    <ListItem
+                        key={item.categoria}
+                        sx={{
+                            bgcolor: '#1e1e1e',
+                            borderRadius: 2,
+                            mb: 1,
+                            p: 2,
+                        }}
+                    >
+                        <ListItemText
+                            primary={item.categoria}
+                            secondary={`Quantidade: ${item.quantidade}`}
+                            sx={{ color: 'inherit' }} // Define a cor do texto como padrão (branco neste caso)
+                        />
+                    </ListItem>
+                ))}
+            </List>
+        </Container>
+    )
 }
 
 export default Report
